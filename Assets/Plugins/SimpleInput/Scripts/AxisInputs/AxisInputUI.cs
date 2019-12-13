4bb781b8f2a510b4d7955eddb5b4ac8ba53704ae
ginkokuryuu <nmh.habib.y@gmail.com>
2019-12-13 14:33:27 +0700

    init

---
+++ Assets/Plugins/SimpleInput/Scripts/AxisInputs/AxisInputUI.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityStandardAssets.Vehicles.Car;
+
+namespace SimpleInputNamespace
+{
+	public class AxisInputUI : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+	{
+		public SimpleInput.AxisInput axis = new SimpleInput.AxisInput();
+		public float value = 1f;
+        public static AxisInputUI instance = null;
+        CarUserControl car;
+
+		private void Awake()
+		{
+			Graphic graphic = GetComponent<Graphic>();
+			if( graphic != null )
+				graphic.raycastTarget = true;
+		}
+
+        public void SetPlayer(CarUserControl _car)
+        {
+            car = _car;
+        }
+
+		private void OnEnable()
+		{
+			axis.StartTracking();
+		}
+
+		private void OnDisable()
+		{
+			axis.StopTracking();
+		}
+
+		public void OnPointerDown( PointerEventData eventData )
+		{
+			axis.value = value;
+		}
+
+		public void OnPointerUp( PointerEventData eventData )
+		{
+			axis.value = 0f;
+		}
+	}
+}
---
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -0,0 +1,101 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using SimpleInputNamespace;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof (CarController))]
+    public class CarUserControl : MonoBehaviour
+    {
+        [SerializeField] Transform spawnPoint = null;
+        public GameObject[] pointCanvas;
+        float timeCD = 5f;
+        bool isShowing = false;
+
+        Rigidbody rb;
+
+        private CarController m_Car; // the car controller we want to use
+
+        private void Awake()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+            rb = GetComponent<Rigidbody>();
+        }
+
+
+        private void FixedUpdate()
+        {
+
+            // pass the input to the car!
+            float h = SimpleInput.GetAxis("Horizontal");
+            float v = SimpleInput.GetAxis("Vertical");
+#if !MOBILE_INPUT
+            float handbrake = 0f;
+            if (Input.GetKey(KeyCode.Space))
+            {
+                handbrake = 1f;
+            }
+            else
+            {
+                handbrake = 0f;
+            }
+            m_Car.Move(h, v, v, handbrake);
+#else
+            m_Car.Move(h, v, v, 0f);
+#endif
+        }
+
+        private void Update()
+        {
+            if (transform.position.y < -5f)
+            {
+                transform.position = spawnPoint.position;
+                rb.velocity = Vector3.zero;
+            }
+
+            if (isShowing)
+            {
+                timeCD -= Time.deltaTime;
+                if(timeCD < 0f)
+                {
+                    isShowing = false;
+                    CloseInfo();
+                    timeCD = 5f;
+                }
+            }
+
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.collider.CompareTag("Respawn"))
+            {
+                transform.position = spawnPoint.position;
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Point"))
+            {
+                OpenInfo(other.gameObject.name[0] - '0');
+            }
+        }
+
+        void OpenInfo(int index)
+        {
+            pointCanvas[0].SetActive(true);
+            isShowing = true;
+        }
+
+        void CloseInfo()
+        {
+            foreach(GameObject g in pointCanvas)
+            {
+                g.SetActive(false);
+            }
+        }
+    }
+}
